27800ea5d16bf52302a23c1ac5a4cf545c17924b
Brayan Landa <landakkpp@gmail.com>
2026-02-27 16:50:59 -0500

    feat: add contracts events

---
+++ src/Shared.Contracts/Events/Auth/Authevents.cs
@@ -0,0 +1,28 @@
+namespace Shared.Contracts.Events.Auth;
+
+public record OrderValidationRequest
+{
+    public Guid CorrelationId { get; set; }
+    public Guid OrderId { get; set; }
+    public Guid UserId { get; set; }
+    public string UserEmail { get; set; } = default!;
+    public DateTime Timestamp { get; set; } = DateTime.Now;
+}
+
+public record UserValidated
+{
+    public Guid CorrelationId { get; set; }
+    public Guid OrderId { get; set; }
+    public Guid UserId { get; set; }
+    public string UserEmail { get; set; } = default!;
+    public DateTime Timestamp { get; set; } = DateTime.Now;
+}
+
+public record UserRejected
+{
+    public Guid CorrelationId { get; init; }
+    public Guid OrderId       { get; init; }
+    public Guid UserId        { get; init; }
+    public string Reason      { get; init; } = default!;
+    public DateTime Timestamp { get; init; } = DateTime.UtcNow;
+}
