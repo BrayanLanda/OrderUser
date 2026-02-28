f03b6a59b5849430b3ecdb7cdf28a221b4994311
Brayan Landa <landakkpp@gmail.com>
2026-02-28 00:52:20 -0500

    feat: add dtos in application

---
+++ src/Auth.Application/Repositories/IJwtService.cs
@@ -0,0 +1,6 @@
+namespace Auth.Application.Repositories;
+
+public interface IJwtService
+{
+    
+}
---
+++ src/Auth.Application/UseCases/RegisterUseCase.cs
@@ -0,0 +1,6 @@
+namespace Auth.Application.UseCases;
+
+public class RegisterUseCase
+{
+    
+}
